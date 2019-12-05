52157a2e4ff7a95ca6515f3ceca55872fa580f60
mushipeas <8151412+mushipeas@users.noreply.github.com>
2019-12-05 18:13:42 +0000

    testing with inbuilt sorted()

--- __main__.py
+++ __main__.py
@@ -3,17 +3,23 @@ from random import randint
 
 from bubblesort import Bubble_sort as bsort
 from insertionsort import Insertion_sort as isort
+from selectionsort import Selection_sort as ssort
 
 def trysort(sort_method):
     seed(50)
     input_array = [randint(0, 10) for x in range(20)]
+    sorted_builtin = sorted(input_array)
 
-    print('Unsorted: {}'.format(input_array))
     sort_method(input_array)
-    print('Sorted:   {}'.format(input_array))
+
+    resolution = 'correctly sorted' if sorted_builtin == input_array else 'did NOT sort'
+    print('Method {}.'.format(resolution))
 
 print('Insertion Sort:')
 trysort(isort().sort)
 
 print('Bubble Sort:')
 trysort(bsort().sort)
+
+print('Selection Sort:')
+trysort(ssort().sort)
