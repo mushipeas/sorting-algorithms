b88b08e31f67a5bd1646bdeee9a226019617ac8b
mushipeas <8151412+mushipeas@users.noreply.github.com>
2019-12-08 14:22:15 +0000

    fix infinite recursion issue

--- quicksort.py
+++ quicksort.py
@@ -1,32 +1,27 @@
- 
-class Quick_sort:
-    def sort(self, items, left=0, right=None):
-        if not right: right = len(items) - 1
-        print(sum(items))
-        if left < right and right - left > 1:
-            pivot = self.select_pivot(items, left, right)
-            p = self.partition(items, left, right, pivot)
-            self.sort(items, left, p)
-            self.sort(items, p+1, right)
+class Quick_sort():
+    def sort(self, items, left = 0, right = None):
+        if right == None: right = len(items) - 1
+
+        if left < right:
+            pivot_ind = self.select_pivot(items, left, right)
+            i = self.partition(items, left, right, pivot_ind)
+
+            self.sort(items, left, i-1)
+            self.sort(items, i+1, right)
         return None
 
-    def partition(self, items, curr_left, curr_right, pivot):
-        while curr_left < curr_right:
-            if curr_right == pivot: curr_right -= 1
-            if curr_left == pivot: curr_left += 1
-            
-            if items[curr_left] <= items[pivot]:
-                curr_left += 1
-            if items[curr_right] > items[pivot]:
-                curr_right -= 1
-            
 
-            if curr_right > 0 and curr_left < curr_right:
-                self.swap(items, curr_left, curr_right)
-                curr_left += 1
-                curr_right -= 1
-        self.swap(items, pivot, curr_left)
-        return curr_left
+    def partition(self, items, left, right, pivot_ind):
+        if not pivot_ind == right: self.swap(items, pivot_ind, right)
+        pivot = items[right]
+
+        i = left - 1
+        for j in range(left, right + 1):
+            if items[j] <= pivot:
+                i += 1
+                self.swap(items, i, j)
+        # self.swap(items, i, right)
+        return i
 
     def select_pivot(self, items, left, right):
         return right
@@ -34,3 +29,27 @@ class Quick_sort:
     def swap(self, items, j, k):
         items[j], items[k] = items[k], items[j]
         return None
+
+
+if __name__ == "__main__":
+    
+    from random import randint
+    
+    qs = Quick_sort()
+
+    # items = [6, 1, 3, 2, 9, 5, 1, 10, 6, 7, 3, 2, 6, 8, 2, 2, 2, 0, 10, 1]
+    items = [10, 7, 8, 9, 1, 5]
+    print('Original: ' + str(items))
+    qs.sort(items)
+    print('Sorted:   ' + str(items))
+    
+    items = [randint(0,10) for x in range(20)]
+    print('Original: ' + str(items))
+    qs.sort(items)
+    print('Sorted:   ' + str(items))
+
+    items = [randint(0,10) for x in range(20)]
+    print('Original: ' + str(items))
+    qs.sort(items)
+    print('Sorted:   ' + str(items))
+    
