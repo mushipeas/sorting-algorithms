06cc5a688ec6dd0bbea53630177239bb18b9e49a
mushipeas <8151412+mushipeas@users.noreply.github.com>
2019-12-09 17:34:11 +0000

    improve variable naming

--- mergesort.py
+++ mergesort.py
@@ -5,26 +5,27 @@ class Merge_sort:
         items.clear()
         items.extend(sorted_list)
 
-    def sort_helper(self, items):
-        if len(items) <= 1: return items
+    def sort_helper(self, sub_arr):
+        if len(sub_arr) <= 1: return sub_arr
 
-        left =  self.sort_helper(items[ :(len(items)//2) ])
-        right = self.sort_helper(items[ (len(items)//2): ])
+        mid = len(sub_arr) // 2
+        left =  self.sort_helper(sub_arr[ :mid ])
+        right = self.sort_helper(sub_arr[ mid: ])
 
         return self.merge(left, right)
 
     def merge(self, left, right):
-        sorted_subarray = []
+        sorted_sub_arr = []
         i = 0
         for j in range(len(left)):
             while i < len(right) and right[i] < left[j]:
-                sorted_subarray.append(right[i])
+                sorted_sub_arr.append(right[i])
                 i += 1
-            sorted_subarray.append(left[j])
+            sorted_sub_arr.append(left[j])
 
-        if i < len(right): sorted_subarray += right[i:]
+        if i < len(right): sorted_sub_arr.extend(right[i:])
 
-        return sorted_subarray
+        return sorted_sub_arr
 
 
 if __name__ == "__main__":
