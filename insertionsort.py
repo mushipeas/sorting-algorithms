9146db43f60f4409352348f204a01d65476c7eda
mushipeas <8151412+mushipeas@users.noreply.github.com>
2019-12-19 06:37:06 +0000

    add test code

--- insertionsort.py
+++ insertionsort.py
@@ -8,3 +8,27 @@ class Insertion_sort(Inplace_sort):
             while j and self.items[j] < self.items[j-1]:
                 self.swap(j, j-1)
                 j -= 1
+
+        return None
+
+if __name__ == "__main__":
+    
+    from random import randint
+    
+    qs = Insertion_sort()
+
+    items = [6, 1, 3, 2, 9, 5, 1, 10, 6, 7, 3, 2, 6, 8, 2, 2, 2, 0, 10, 1]
+    print('Original: ' + str(items))
+    qs(items)
+    print('Sorted:   ' + str(items))
+    
+    items = [randint(0,10) for x in range(20)]
+    print('Original: ' + str(items))
+    qs(items)
+    print('Sorted:   ' + str(items))
+
+    items = [randint(0,10) for x in range(20)]
+    print('Original: ' + str(items))
+    qs(items)
+    print('Sorted:   ' + str(items))
+    
